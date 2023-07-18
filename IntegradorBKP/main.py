3d5f098f85de08461983102feabfebe18de0c106
gustavoalvise <51672104+gustavoalvise@users.noreply.github.com>
2023-07-17 22:48:18 -0300

    0.6.2
    Beginning to write backup target arrays and file system manipulation

--- IntegradorBKP/main.py
+++ IntegradorBKP/main.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import os.path
+import os
+import datetime
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -8,6 +10,20 @@ from google.oauth2.credentials import Credentials
 # If modifying these scopes, delete the file token.json.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
+# Obter informações de data e hora da modificação do arquivo
+def obter_data_mod_arquivo(caminho_arquivo):
+    data_mod_timestamp = os.path.getmtime(caminho_arquivo)
+    data_mod = datetime.datetime.fromtimestamp(data_mod_timestamp)
+    return data_mod
+
+# Obter o arquivo mais recente dentro de uma pasta
+
+def obter_arquivo_mais_recente(caminho_pasta):
+    arquivos = os.listdir(caminho_pasta)
+    arquivos = [arquivo for arquivo in arquivos if os.path.isfile(os.path.join(caminho_pasta, arquivo))]
+    data_mod_arquivos = [(arquivo, os.path.getmtime(os.path.join(caminho_pasta, arquivo))) for arquivo in arquivos]
+    arquivo_mais_recente = max(data_mod_arquivos, key=lambda x: x[1])[0]
+    return arquivo_mais_recente
 
 def main():
     creds = None
@@ -31,7 +47,6 @@ def main():
     
     # Fim da parte de autenticação do acesso
 
-    # Call the Sheets API
     # Ler informações do Google Sheets
     sheet = service.spreadsheets()
     result = sheet.values().get(spreadsheetId='1o5XkucKJXtlM4eaG3IFWLBJCpj2Keoj4OKWGDgHRdFE',
@@ -44,13 +59,28 @@ def main():
     range_name = 'BKPNuvem!B:B'
     result = service.spreadsheets().values().get(
     spreadsheetId='1o5XkucKJXtlM4eaG3IFWLBJCpj2Keoj4OKWGDgHRdFE', range=range_name).execute()
-    values = result.get('values', [])
+    valuesraw = result.get('values', [])
 
-    vetor = [value[0] for value in values if value]
+    vetor_clientes = [value[0] for value in valuesraw if value]
     
-    print(vetor)
-       
+    print(vetor_clientes)
+
+    # Começo de teste de leitura de arquivos
+
+    # Pegando o arquivo mais recente
+
+    caminho_pasta = f'//192.168.1.211/ftp/{vetor_clientes[1]}/Bkp$implesDelphi'
+    arquivo_recente = obter_arquivo_mais_recente(caminho_pasta)
+
+    print(f"O arquivo mais recente na pasta é: {arquivo_recente}")
+
+    # Pegando a data de modificação do arquivo mais recente
     
+    caminho_arquivo = caminho_pasta
+    data_mod = obter_data_mod_arquivo(caminho_arquivo)
+    print(f"A data de modificação do arquivo é: {data_mod}")
+
+
 """
     # adicionar/editar valores no Google Sheets
     valores_adicionar = [
